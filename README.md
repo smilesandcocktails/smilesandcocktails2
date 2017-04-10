52581e88436eb3fc08b261fc73d079d2e9950ba1
Cara Chew <carachew@gmail.com>
2017-04-11 00:21:56 +0800

    SET UP Server page & Model

---
+++ README.md
@@ -0,0 +1,10 @@
+sense of identity
+better communication between developers and home owners
+value added service
+marketing value add
+defects management
+handover kit
+documentation pertaining to project and unit handover
+
+eventually be the GO-TO HOME app, where you can link your routes from home to other places
+(gothere.sg), transport, and others
---
+++ server.js
@@ -0,0 +1,47 @@
+var express = require('express')
+var app = express()
+var port = process.env.PORT || 8080
+
+var mongoose = require('mongoose')
+var unit = require('./controllers/unitscontroller')
+
+var dburi = process.env.PROD_MONGODB || 'mongodb://localhost:27017/test'
+mongoose.connect(dburi)
+mongoose.Promise = global.Promise
+
+var path = require('path')
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'))
+db.once('open'), function() {
+  console.log('WE ARE LIVE!');
+}
+
+var bodyParser = require('body-parser')
+var methodOverride = require('method-override')
+var ejsLayouts = require('express-ejs-layouts')
+
+app.set('view engine', 'ejs')
+
+app.use(ejsLayouts)
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json())
+app.use(methodOverride('_method'))
+app.use(express.static(path.join(__dirname,'public')))
+
+
+app.get('/', function(req,res) {
+  res.send('homepage')
+})
+
+app.get('/about', function(req,res) {
+  res.send('about')
+})
+
+app.get('/contact', function(req,res) {
+  res.send('contact')
+})
+
+app.listen(port, function(){
+  console.log('Express is running on port ' + port);
+})
