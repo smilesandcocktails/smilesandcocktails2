6fd6f40ba420db3aff4da3f20a8053699dc315e8
Cara Chew <carachew@gmail.com>
2017-04-11 00:52:07 +0800

    Commented out models to try heroku

--- models/unit.js
+++ models/unit.js
@@ -1,16 +1,16 @@
-var mongoose = require ('mongoose')
-
-var unitSchema = new mongoose.Schema ({
-  name: String,
-  password: String,
-  email: String,
-  projectname: String,
-  block: Number,
-  floor: Number,
-  stack: Number,
-  data: String
-})
-
-var Unit = mongoose.model('Unit', unitSchema)
-
-module.exports = Unit
+// var mongoose = require ('mongoose')
+//
+// var unitSchema = new mongoose.Schema ({
+//   name: String,
+//   password: String,
+//   email: String,
+//   projectname: String,
+//   block: Number,
+//   floor: Number,
+//   stack: Number,
+//   data: String
+// })
+//
+// var Unit = mongoose.model('Unit', unitSchema)
+//
+// module.exports = Unit
